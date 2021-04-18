3b11007de0a51a8f70ab89b1fdbb5a93f475a960
cb-bharathvaj <bharathvaj@chargebee.com>
2021-04-18 17:41:55 +0530

    fix: improvements

--- extension/js/content.js
+++ extension/js/content.js
@@ -1,13 +1,13 @@
-const getBundlephobiaLink = package => `https://bundlephobia.com/result?p=${package}`
+const getBundlephobiaLink = (package) => `https://bundlephobia.com/result?p=${package}`;
+const getBundlephobiaAPI = (package) => `https://bundlephobia.com/api/size?package=${package}`;
+const errorHandler = (err) => console.warn('Retrieving JS bundle size failed', { err });
 
 const sizeTransformer = ({ gzip, size }) => {
-  const parse = input => parseFloat(input).toFixed(1)
-  const format = input => input > 1048576
-    ? `${parse(input / 1048576)} MB` : input > 1024
-    ? `${parse(input / 1024)} KB` : `${input} B`
+  const parse = (input) => parseFloat(input).toFixed(1);
+  const format = (input) => (input > 1048576 ? `${parse(input / 1048576)} MB` : input > 1024 ? `${parse(input / 1024)} KB` : `${input} B`);
 
   return {
     gzip: format(gzip),
     size: format(size),
-  }
-}
+  };
+};
